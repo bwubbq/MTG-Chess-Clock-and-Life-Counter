b1124eb606d352d40f11c5e5377c812921fa40b3
bwubbq <wu_561@hotmail.com>
2023-03-21 19:35:57 +1100

    changes to sizing and to directional flow
    added dynamic sizing to each player space and added a way to reverse player flow

--- mtg chess clock/script.js
+++ mtg chess clock/script.js
@@ -8,6 +8,9 @@ const stopTimerButton = document.querySelector('.stop-timer');
 // Keep track of the intervals for each player
 const intervals = [null, null, null, null];
 
+//Track of anti-clockwise button
+let antiClockwiseOn = false;
+
 // Loop through each button and add an event listener
 buttons.forEach(button => {
   button.addEventListener('click', () => {
@@ -21,7 +24,7 @@ buttons.forEach(button => {
     // Increase or decrease the life total based on the button clicked
     if (button.classList.contains('plus-life')) {
       lifeTotal += 1;
-    } else {	
+    } else {
       lifeTotal -= 1;
     }
 
@@ -32,17 +35,11 @@ buttons.forEach(button => {
 
 // Loop through each start timer button and add an event listener
 startTimerButtons.forEach((startTimerButton, index) => {
-  // Start the stopwatch when the start timer button is clicked
   startTimerButton.addEventListener('click', () => {
-    // If the stopwatch is already running, stop it
-    if (intervals[index]) {
-      clearInterval(intervals[index]);
-    }
-
     // Set the start time to the current time
     const startTime = new Date().getTime();
 
-    // Update the elapsed time every 10 milliseconds
+    // Set the interval ID in the intervals array
     intervals[index] = setInterval(() => {
       const elapsedTime = new Date().getTime() - startTime;
       const minutes = Math.floor(elapsedTime / 60000);
@@ -52,26 +49,41 @@ startTimerButtons.forEach((startTimerButton, index) => {
       // Update the elapsed time element with the new value
       elapsedTimes[index].textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(3, '0')}`;
     }, 10);
+
+    // Add the "priority" class to the current player element
+    const currentPlayerEl = startTimerButton.closest('.player');
+    currentPlayerEl.classList.add('priority');
   });
 });
 
+//listener for anticlockwise toggle
+const antiClockwiseButton = document.querySelector('.anti-clockwise');
+antiClockwiseButton.addEventListener('click', () => {
+  antiClockwiseOn = !antiClockwiseOn;
+  console.log(`Anti-clockwise is now ${antiClockwiseOn}`);
+});
+
 // Loop through each pass priority button and add an event listener
-passPriorityButtons.forEach((passPriorityButton, index) => {
-  passPriorityButton.addEventListener('click', () => {
+let index = 0;
+passPriorityButtons.forEach((button, i) => {
+  button.addEventListener('click', () => {
+    console.log('passPriority clicked'); // added console log
     // Stop the timer for the current player
     clearInterval(intervals[index]);
 
     // Get the current player element and the next player element
-    const currentPlayerEl = passPriorityButton.closest('.player');
-    const nextPlayerEl = currentPlayerEl.nextElementSibling || currentPlayerEl.parentNode.firstElementChild;
+    const currentPlayerEl = button.closest('.player');
+    const nextPlayerEl = antiClockwiseOn ? currentPlayerEl.previousElementSibling || currentPlayerEl.parentNode.lastElementChild : currentPlayerEl.nextElementSibling || currentPlayerEl.parentNode.firstElementChild;
 
     // Add the "priority" class to the next player element and remove it from the current player element
     nextPlayerEl.classList.add('priority');
     currentPlayerEl.classList.remove('priority');
 
+    // Update the index to the next player
+    index = antiClockwiseOn ? (i + 3) % 4 : (i + 1) % 4;
+
     // Start the timer for the next player
-    const nextIndex = (index + 1) % 4;
-    startTimerButtons[nextIndex].click();
+    startTimerButtons[index].click();
   });
 });
 
